3b77843a889ed8c89c84142370935adae694ae44
Matthew Roberts <mattleeroberts@gmail.com>
2026-03-13 07:50:56 -0700

    sync: auto-commit 2026-03-13T14:50:56.022Z

--- app/src/main/java/com/debuginstruments/app/MainActivity.kt
+++ app/src/main/java/com/debuginstruments/app/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.debuginstruments.app
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("MainActivity", "Test log: MainActivity created")
     }
 }
